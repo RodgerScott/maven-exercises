2af74f6e254b52b2beea043683799a013cc61239
RodgerScott <rodger.scott@gmail.com>
2018-03-26 11:35:41 -0500

    added methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.SystemUtils;
 
 import java.util.Scanner;
 
@@ -15,5 +16,21 @@ public class Main {
         }
         System.out.println("Flipped Case: " + StringUtils.swapCase(entry));
         System.out.println("Reversed: " + StringUtils.reverse(entry));
+        System.out.println(StringUtils.containsAny(entry, "aeiou"));
+    }
+
+    public static boolean stringIsANumber (String input) {
+        return StringUtils.isNumeric(input);
+    }
+
+    public static String flipTheCase (String input) {
+        return StringUtils.swapCase(input);
+    }
+
+    public static String reverse (String input) {
+        return StringUtils.reverse(input);
     }
 }
+
+
+
